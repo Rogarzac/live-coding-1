bbb0bdc19771fc65238434ce04f57486073d5613
Rogarzac <rogarzac@gmail.com>
2023-10-06 19:28:56 -0600

    Se realizó el ejercicio de live coding con mi compañero Angel, los comentarios estan en el JS

--- app.js
+++ app.js
@@ -1,1 +1,63 @@
-// Escribe tu código aquí.
+/* 1.- Construye una función que reciba por parámetros un string y un array de strings. La función debe retornar un arreglo de strings con los strings que sean de mayor longitud que el string recibido por parámetros.
+
+- Escribe una función que reciba 2 parámetros
+- Parámetro 1 - Un string
+- Parámetro 2 - Un array de strings
+- La función debe devolver todas las palabras del array de entrada (parámetro 2) que sean más largas que el string de entrada (parámetro 1)
+- Ejemplo:
+- `const myArray = ['insecto', 'bootcamp', 'mangos', 'reptil', 'mosca', 'escritorio'];`
+- `bigWords('bocina', myArray);`
+- Output - `['insecto', 'bootcamp', 'escritorio']` 
+
+var string = "árbol";
+var myArray = ["balon", "ventana", "cama", "tele","computadora"];
+console.log(myArray);
+
+
+
+
+function bigWords(string, myArray){
+var longitud = myArray.;
+console.log(longitud);
+return value
+}
+*/
+//Hasta aqui llegue yo Angel me hizo la retroalimentacion de que necesito practicar mucho los metodos, realmente tiene razon, al mmomento de programar sin recursos si se me cierra el mundo, posteirormente, apoyado con el, pudimos completar el ejercicio:
+const btn = document.querySelector("#btn"); //obtenemos el boton para despues darle un evento click y ejecutar el comando
+const listWord = ['insecto', 'bootcamp', 'mangos', 'reptil', 'mosca', 'escritorio'];
+
+
+//funcion que devuelve las palabas con mayor longitud que las de entrada
+function bigWords(word, list) {
+    //se usa el metodo filter para generar una nueva cadena   sin las palabras con menor longitud
+    const newList = list.filter(item => {
+        console.log(`word ${word}, item : ${item} `);
+        return word.length < item.length;
+    })
+    return newList
+
+}
+
+
+
+
+/* 2.- Construye una función que reciba por parámetros un array de strings y lo imprima dentro de la lista (<ul></ul>) en el index.html. La función debe mostrar la lista en el HTML manipulando el DOM, cada string del arreglo debe imprimirse en un elemento **<li></li>**. El array que se recibe por parámetros debe ser un array resultante de utilizar la función anterior. */
+
+
+function imprimirLista(strings) {
+    const listElement = document.querySelector("#lista");//obtenemos el elemento para agregar los li
+    //iteramos el array de palabras y creamos el elemento li por cada una de ellas agregandole el texto y agregandolo al elemento ul 
+    strings.forEach(function (string) {
+        const li = document.createElement('li');
+        li.textContent = string;
+        listElement.appendChild(li);
+    });
+}
+
+//usamos el elemento boton para que escuche un evento click  y agregemos el contenido en el html por medio del dom
+btn.addEventListener("click", evet => {
+    //obtenemos el valor de input para usar en la funcion big
+    const palabra = document.querySelector("#palabras").value;
+    const newList = bigWords(palabra, listWord);//obtenemos la nuevas palabras ya filtradas
+    imprimirLista(newList);// agregamos las palabras al html por medio del dom
+})
